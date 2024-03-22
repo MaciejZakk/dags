6b9d63ec589b26ee17e0ec620c5be2287489ca6c
MaciejZakk <macio1223@gmail.com>
2024-03-22 10:05:13 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,38 @@
+from datetime import datetime, timedelta
 from airflow import DAG
-from datetime import datetime
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+import time
 
-def helloWorld():
-    print("Hello World")
+def print_hello():
+    print('Hello Airflow!')
 
-with DAG(dag_id="DAG-1",
-         start_date=datetime(2024,1,1),
-         schedule_interval=timedelta(seconds=10),
-         catchup=False) as dag:
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 3, 22),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
 
-	task1 = PythonOperator(task_id="hello_world",python_callable=helloWorld)
+dag = DAG(
+    'example_dag',
+    default_args=default_args,
+    description='A simple Airflow DAG',
+    schedule_interval=timedelta(seconds=5),
+)
 
-task1
+start = DummyOperator(task_id='start', dag=dag)
+
+hello_operator = PythonOperator(
+    task_id='hello_task',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+end = DummyOperator(task_id='end', dag=dag)
+
+start >> hello_operator >> end
