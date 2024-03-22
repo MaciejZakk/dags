47d0a1fa940f3e8630337f876c97b7ff41224263
MaciejZakk <macio1223@gmail.com>
2024-03-22 10:01:08 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,13 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python_operator import PythonOperator
-from uneven_intervals_timetable import UnevenIntervalsTimetable
 
 def helloWorld():
     print("Hello World")
 
-with DAG(dag_id="hello_world_dag",
+with DAG(dag_id="DAG-1",
          start_date=datetime(2024,1,1),
-         schedule=UnevenIntervalsTimetable(),
+         schedule_interval=timedelta(seconds=10),
          catchup=False) as dag:
 
 	task1 = PythonOperator(task_id="hello_world",python_callable=helloWorld)
