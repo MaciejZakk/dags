aad6f78c3b389c2f09b43e98c97e12110f046da3
MaciejZakk <macio1223@gmail.com>
2024-03-22 09:50:10 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,14 +3,13 @@ from datetime import datetime
 from airflow.operators.python_operator import PythonOperator
 
 def helloWorld():
-    print(‘Hello World’)
+    print("Hello World")
 
 with DAG(dag_id="hello_world_dag",
          start_date=datetime(2021,1,1),
          schedule_interval="@hourly",
          catchup=False) as dag:
-		task1 = PythonOperator(
-        task_id="hello_world",
-        python_callable=helloWorld)
+		 
+	task1 = PythonOperator(task_id="hello_world",python_callable=helloWorld)
 
 task1
